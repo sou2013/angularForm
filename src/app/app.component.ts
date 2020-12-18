585c254fb28bb1a30e1f5628581e7444a656fc3a
nameabc <a@test.com>
2020-12-17 18:15:05 -0800

    init check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyCountry } from './my-country'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularForm';
+
+  /*
+  countryList:country[]=[
+    new country("1", "USA"),
+    new country("2", "India"),
+    new country("3", "England")
+  ]
+  */
+  countryList:MyCountry[]=[
+    new MyCountry('1', 'USA'),
+    new MyCountry('2', 'Taiwan'),
+    new MyCountry('3', 'England')
+  ]
+  onSubmit(contactForm) {
+    console.log(contactForm.value);
+  }
 }
+
+export class country {
+  id:string;
+  name:string;
+ 
+  constructor(id:string, name:string) {
+    this.id=id;
+    this.name=name;
+  }
+
+}
+
+ 
---
+++ src/app/my-country.spec.ts
@@ -0,0 +1,7 @@
+import { MyCountry } from './my-country';
+
+describe('MyCountry', () => {
+  it('should create an instance', () => {
+    expect(new MyCountry()).toBeTruthy();
+  });
+});
